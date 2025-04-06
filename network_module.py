41b0e42b4fcf64bacaa9e0f1540980360c2f3bf3
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2025-04-06 21:50:21 +0530

    network_module.py

--- network_module.py
+++ network_module.py
@@ -260,23 +260,7 @@ class ContextualAttention(nn.Module):
         self.fuse = fuse
         self.use_cuda = use_cuda
         self.device_ids = device_ids
-    def extract_image_patches(images, ksizes, strides, rates, padding='same'):
-        assert len(images.size()) == 4
-        assert padding in ['same', 'valid']
-        batch_size, channel, height, width = images.size()
-        if padding == 'same':
-            images = same_padding(images, ksizes, strides, rates)
-        elif padding == 'valid':
-            pass
-        else:
-            raise NotImplementedError('Unsupported padding type: {}.\
-                Only "same" or "valid" are supported.'.format(padding))
-        unfold = torch.nn.Unfold(kernel_size=ksizes,
-                             dilation=rates,
-                             padding=0,
-                             stride=strides)
-        patches = unfold(images)
-        return patches
+     
           
           
          
